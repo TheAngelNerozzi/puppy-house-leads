314cb94f5043a33f3aae5dbbdf61d64f6c3f744c
Angel Nerozzi <120354479+TheAngelNerozzi@users.noreply.github.com>
2025-09-08 15:34:53 -0400

    Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,6 +2,7 @@ import HeroSection from '@/components/HeroSection';
 import ValueProposition from '@/components/ValueProposition';
 import PuppyGallery from '@/components/PuppyGallery';
 import AboutSection from '@/components/AboutSection';
+import BreedHistorySection from '@/components/BreedHistorySection';
 import TestimonialsSection from '@/components/TestimonialsSection';
 import FinalCTA from '@/components/FinalCTA';
 import Footer from '@/components/Footer';
@@ -14,6 +15,7 @@ const Index = () => {
       <ValueProposition />
       <PuppyGallery />
       <AboutSection />
+      <BreedHistorySection />
       <TestimonialsSection />
       <FinalCTA />
       <Footer />
